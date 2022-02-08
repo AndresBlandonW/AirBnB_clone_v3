31d0bd378ab8064e3dc285aed49fdf204b642147
Andres Blandon <andblandon@hotmail.com>
2022-02-08 01:57:46 +0000

    API

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""index file"""
+from flask import Flask, jsonify
+from api.v1.views import app_views
+
+
+@app.route('/status')
+def status_route():
+"""Return a JSON status"""
+return jsonify({'status': 'OK'})
