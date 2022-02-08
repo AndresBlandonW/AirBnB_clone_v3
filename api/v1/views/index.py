62d91a1af4a477887fd4082c9ecba4071e8d2c20
Andres Blandon <andblandon@hotmail.com>
2022-02-08 02:34:06 +0000

    update error

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify
 from api.v1.views import app_views
 
 
-@app.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status_route():
     """Return a JSON status"""
     return jsonify({'status': 'OK'})
